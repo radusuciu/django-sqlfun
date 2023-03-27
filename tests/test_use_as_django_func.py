c035a3ef30dc7707b0f7784befc1858e6e08b861
Radu Suciu <radusuciu@gmail.com>
2023-03-26 19:33:05 -0700

    Fix issue with test

--- tests/test_use_as_django_func.py
+++ tests/test_use_as_django_func.py
@@ -1,10 +1,13 @@
 import pytest
 
+from django.core.management import call_command
 from test_project.models import BadSum, Foo
 
 
 @pytest.mark.django_db
 def test_bad_sum_with_queryset():
+    call_command('makemigrations')
+    call_command('migrate')
     Foo.objects.create(foo=3)
     annotated_queryset = Foo.objects.annotate(bad_sum_result=BadSum(3, 5))
     result = annotated_queryset.first()
