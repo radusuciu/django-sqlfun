cf980c81e390f76b545612b1d81a4700f489e5ef
Radu Suciu <radusuciu@gmail.com>
2026-07-17 14:52:32 -0700

    fix: hard-fail makemigrations on unresolvable function signatures

--- sqlfun/management/commands/makemigrations.py
+++ sqlfun/management/commands/makemigrations.py
@@ -1,41 +1,30 @@
-
 import django
+from django.core.management.base import CommandError
 from django.core.management.commands.makemigrations import Command as BaseCommand
 
-from sqlfun.parsing import SqlFunParseError
+from sqlfun.naming import SqlFunError
 from sqlfun.utils import make_sqlfun_migrations
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-
         try:
             make_sqlfun_migrations(
                 custom_name=options.get('name'),
                 stdout=self.stdout,
-                is_dry_run=options.get('dry_run')
+                is_dry_run=options.get('dry_run'),
             )
-        except SqlFunParseError as e:
-            self.stderr.write(f'[sqlfun] Could not parse a function signature: {e}')
-            self.stderr.write(
-                '[sqlfun] No sqlfun migration was generated. Fix the SQL '
-                'definition above and re-run makemigrations.'
-            )
+        except SqlFunError as error:
+            raise CommandError(
+                f'[sqlfun] Could not resolve a function signature: {error}\n'
+                '[sqlfun] No sqlfun migration was generated. Fix the SQL definition '
+                'above and re-run makemigrations.'
+            ) from error
         except django.db.utils.ProgrammingError:
             self.stderr.write(
                 '[sqlfun] It seems like the SqlFunDefinition model does not exist yet. '
                 'A migration will be generated which must be applied before you can '
                 'define custom functions.'
             )
-            pass
-        except Exception as e:
-            self.stderr.write(
-                '[sqlfun] Could not make migrations for sqlfun functions. '
-                'Is the SqlFunDefinition model created properly?'
-            )
-            if options.get('verbosity', 0) > 0:
-                self.stderr.write(f'Exception details: {e}')
-                import traceback
-                traceback.print_exc(file=self.stderr)
 
         return super().handle(*args, **options)
